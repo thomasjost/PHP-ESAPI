476e0cf06c1aa6d999be35e16ca55f899f221bc9
Renaat De Muynck <renaat.demuynck@gmail.com>
2015-03-26 17:01:41 +0100

    Split files with multiple classes
    There should be only one class per file

---
+++ src/Threshold.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ * 
+ * PHP version 5.2
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   SVN: $Id$
+ * @link      http://www.owasp.org/index.php/ESAPI
+ */
+
+/**
+ * Models a simple threshold as a count and an interval, along with a set of
+ * actions to take if the threshold is exceeded.
+ * 
+ * These thresholds are used to define when the accumulation of a particular
+ * event has met a set number within the specified time period. Once a threshold
+ * value has been met, various actions can be taken at that point.
+ *
+ * PHP version 5.2
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @author    Jeff Williams <jeff.williams@aspectsecurity.com>
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @author    jah <jah@jahboite.co.uk>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   Release: @package_version@
+ * @link      http://www.owasp.org/index.php/ESAPI
+ */
+class Threshold
+{
+
+    /** The name of this threshold. */
+    public $name = null;
+
+    /** The count at which this threshold is triggered. */
+    public $count = 0;
+
+    /** 
+     * The time frame within which 'count' number of actions has to be detected in 
+     * order to trigger this threshold.
+     */
+    public $interval = 0;
+
+    /** 
+     * The list of actions to take if the threshold is met. It is expected that 
+     * this is a list of Strings, but your implementation could have this be a 
+     * list of any type of 'actions' you wish to define. 
+     */
+    public $actions = null;
+
+    /**
+     * Constructs a threshold that is composed of its name, its threshold count, 
+     * the time window for the threshold, and the actions to take if the threshold 
+     * is triggered.
+     * 
+     * @param string $name     The name of this threshold.
+     * @param int    $count    The count at which this threshold is triggered.
+     * @param int    $interval The time frame within which 'count' number of actions
+     *                         has to be detected in order to trigger this threshold.
+     * @param array  $actions  The list of actions to take if the threshold is met.
+     * 
+     * @return Does not return a value.
+     */
+    function __construct($name, $count, $interval, $actions)
+    {
+        $this->name = $name;
+        $this->count = $count;
+        $this->interval = $interval;
+        $this->actions = $actions;
+    }
+
+}
---
+++ src/reference/Event.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ *
+ * PHP version 5.2
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
+ *
+ * @category  OWASP
+ * @package   ESAPI_Reference
+ * @author    Jeff Williams <jeff.williams@aspectsecurity.com>
+ * @author    jah <jah@jahboite.co.uk>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   SVN: $Id$
+ * @link      http://www.owasp.org/index.php/ESAPI
+ */
+
+/**
+ * Reference implementation of an Intrusion Event.
+ *
+ * Represents the count of and times at which a user generated an event that
+ * corresponds to a defined IntrusionDetector threshold.  The intrusion detector
+ * stores instances of events and invokes their increment method which
+ * determines whether the corresponding threshold has been reached.
+ *
+ * @category  OWASP
+ * @package   ESAPI_Reference
+ * @author    Jeff Williams <jeff.williams@aspectsecurity.com>
+ * @author    jah <jah@jahboite.co.uk>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   Release: @package_version@
+ * @link      http://www.owasp.org/index.php/ESAPI
+ */
+class Event
+{
+    private $_key;
+    private $_times = array();
+
+    /**
+     * @var int $count The number of times this event occurred for a given user.
+     */
+    public $count = 0;
+
+    /**
+     * Constructor stores the supplied key as the event name.
+     *
+     * @param string $key A name by which the event is known e.g.
+     *                   'IntegrityException'.
+     *
+     * @return null
+     */
+    public function __construct($key)
+    {
+        $this->_key = $key;
+    }
+
+    /**
+     * The increment method increments the number of times this event occurred
+     * for this user.
+     *
+     * Each time increment is called it will decide whether or not to throw an
+     * IntrusionException based on the supplied count and interval parameters.
+     * If $count is exceeded within $interval seconds then the exception will be
+     * thrown.  This implementation maintains a kind of sliding window of
+     * timestamps so that it can track event occurrences over time.
+     *
+     * @param int $count    The event count that will trigger Intrusion Detection
+     *                      within the supplied interval.
+     * @param int $interval The number of seconds within which the supplied quota of
+     *                      event occurrences will trigger Intrusion Detection.
+     *
+     * @return null
+     */
+    public function increment($count, $interval)
+    {
+        $now = null;
+        if (function_exists('microtime')) {
+            $now = microtime(true);
+            $interval = (float) $interval;
+        } else {
+            $now = time();
+        }
+
+        $this->count++;
+        array_push($this->_times, $now);
+
+        // if the threshold has been exceeded
+        while (sizeof($this->_times) > $count) {
+            array_shift($this->_times);
+        }
+
+        if (sizeof($this->_times) == $count) {
+            $past = reset($this->_times);
+            if ($past === false) {
+                // this should not happen because events are validated in
+                // SecurityConfiguration...
+                $past = $now;
+            }
+            $present = $now;
+            if ($present - $past < $interval) {
+                throw new IntrusionException(
+                    "Threshold exceeded",
+                    "Exceeded threshold for " . $this->_key
+                );
+            }
+        }
+    }
+
+}
